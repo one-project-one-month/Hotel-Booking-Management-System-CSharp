240d6d4da4677e541b37953af766059214fdd6de
Yu Nilar Thinn <yunilarthinn97@gmail.com>
2025-05-19 22:25:26 +0630

    refector: Added Global Exception Handler for the exception for primitive type exception(non-generic)

---
+++ HotelManagementSystem/Helpers/GlobalExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using HotelManagementSystem.Service.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace HotelManagementSystem.Helpers
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var (statusCode, message) = GetExceptionDetails(exception);
+            _logger.LogError(exception, message);
+            httpContext.Response.StatusCode = (int)statusCode;
+            await httpContext.Response.WriteAsJsonAsync(message, cancellationToken);
+
+            return true;
+        }
+        private (HttpStatusCode statusCode, string message) GetExceptionDetails(Exception exception)
+        {
+            return exception switch
+            {
+                UserNotFoundException => (HttpStatusCode.Unauthorized, exception.Message),
+
+                _ => (HttpStatusCode.InternalServerError, $"An unexpected error occurred: {exception.Message}")
+            };
+        }
+    }
+}
