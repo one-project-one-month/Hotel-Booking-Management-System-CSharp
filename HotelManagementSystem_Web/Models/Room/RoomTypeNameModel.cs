f9903d7523d8f9e96629473b16770558f791a019
Nyan Oak Soe <nyanoaksoe77@gmail.com>
2025-06-16 15:43:28 +0630

    refactor: merge code update

---
+++ HotelManagementSystem_Web/Models/Room/RoomTypeNameModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HotelManagementSystem_Web.Models.Room
+{
+    public class RoomType
+    {
+        public string? RoomTypeId { get; set; }
+        public string RoomTypeName { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string RoomImg { get; set; } = null!;
+        public string RoomImgMimeType { get; set; } = null!;
+    }
+
+
+    public class RoomResponse
+    {
+        public List<RoomType> RoomList { get; set; } = new();
+        public string RespCode { get; set; }
+        public string RespDescription { get; set; }
+    }
+
+}
---
+++ HotelManagementSystem_Web/Models/Room/SearchRoomRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelManagementSystem_Web.Models.Room
+{
+    public class SearchRoomRequestModel
+    {
+        public Guid? RoomTypeId { get; set; }
+
+        public DateTime? CheckInDate { get; set; }
+
+        public DateTime? CheckOutDate { get; set; }
+
+        public int? GuestLimit { get; set; } = 1;
+
+    }
+}
