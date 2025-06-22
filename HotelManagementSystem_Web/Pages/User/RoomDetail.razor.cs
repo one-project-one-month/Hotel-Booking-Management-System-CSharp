e2b3f7f2ce18562d091539a4c55c1b55efbbcf7a
sanneiphyo-portedi <sanneiphyo@gmail.com>
2025-06-22 20:02:11 +0630

    fixed Roomdetails

--- HotelManagementSystem_Web/Pages/User/RoomDetail.razor.cs
+++ HotelManagementSystem_Web/Pages/User/RoomDetail.razor.cs
@@ -1,3 +1,4 @@
+using HotelManagementSystem_Web.Layout.Compoments;
 using HotelManagementSystem_Web.Models.Room;
 using Microsoft.AspNetCore.Components;
 using Newtonsoft.Json;
@@ -6,57 +7,39 @@ namespace HotelManagementSystem_Web.Pages.User;
 
 public partial class RoomDetail : ComponentBase
 {
+   
     [Parameter]
-    public Guid RoomId { get; set; }
-
-    private RoomTypeModel? RoomDetails = null;
-
-        private bool isLoading = true; 
-    private bool isDataEmpty = false; 
+    public string RoomTypeId { get; set; }
+    public RoomTypeModel Model { get; set; } = new RoomTypeModel();
+    public RoomTypeModel RoomDetails { get; set; } = new RoomTypeModel();
+    public bool isLoading { get; set; } = false;
+    private AppModal Modal;
+    private Guid _roomTypeId;
 
     protected override async Task OnInitializedAsync()
     {
-        await LoadRoomDetail();
+        isLoading = true;
+        await GetRoomTypeById();
+        isLoading = false;
     }
 
-    private async Task LoadRoomDetail()
+    public async Task GetRoomTypeById()
     {
-        isLoading = true;
-        isDataEmpty = false;
-        try
+        var res = await _httpClient.GetAsync($"api/RoomType/{RoomTypeId}");
+        if (res.IsSuccessStatusCode)
         {
-            var res = await _httpClient.GetAsync("api/RoomType/getroomtypes");
-            if (res.IsSuccessStatusCode)
+            var jsonStr = await res.Content.ReadAsStringAsync();
+            var resModel = JsonConvert.DeserializeObject<RoomTypeResModel>(jsonStr);
+            if (resModel.respCode == "200")
             {
-                var jsonStr = await res.Content.ReadAsStringAsync();
-                var resModel = JsonConvert.DeserializeObject<RoomTypeListResModel>(jsonStr);
-
-                if (resModel?.respCode == "200")
-                {
-                    RoomDetails = resModel.RoomTypeList.FirstOrDefault(r => r.RoomTypeId == RoomId);
-                }
-
-                if (RoomDetails == null)
-                {
-                    isDataEmpty = true;
-                }
-                else
-                {
-                    isDataEmpty = false; // API responded but no valid data
-                }
-
-
+                Model = resModel.RoomType;
+                var editModel = JsonConvert.SerializeObject(Model);
+                RoomDetails = JsonConvert.DeserializeObject<RoomTypeModel>(editModel);
             }
         }
-        catch (Exception e)
+        else
         {
-            Console.WriteLine(e.Message);
-            isDataEmpty = true; // error treat as no data
+            Console.WriteLine(JsonConvert.SerializeObject(res));
         }
-        finally
-        {
-            isLoading = false;
-        }
-        
     }
 }
